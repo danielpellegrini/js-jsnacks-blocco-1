52bb7d4760b73100223a659cfbe5f0d531c56e08
Daniel <danielpellegrini87@gmail.com>
2020-12-18 16:48:55 +0100

    COMPLETED

---
+++ blocco-1/9/js/script.js
@@ -0,0 +1,9 @@
+// Calcola la somma e la media dei primi 10 numeri.
+
+var sum = 0
+
+for (var i = 0; i <= 10; i++) {
+  sum += parseInt(i);
+}
+console.log('the sum is: ' + sum);
+console.log('the avarage is: ' + sum / 10);
